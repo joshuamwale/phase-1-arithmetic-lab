748f286d34cfb038fb8796803f38ba540046c20b
Joshua <mwalejoshua89@gmail.com>
2022-06-11 21:26:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let num1= 31, num2 = 2;
-31 * 2 = 62
+let multiply = (num1 * num2 = 62);
+num1= 31, num2 = 2;
+31 * 2;
+//=> 62
 
-// Write your code here
+Math.random();
+//=> 0.4495507082209371
+
+let mod = 4 % (num3/num4);
+//=> 4
+
+Math.max (10, 15, 20, 19);
+//=> 20
+
